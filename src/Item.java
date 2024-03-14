044b9532de60c108c98d10f02da7a50720a075dd
khanhson301104 <52119462+khanhson301104@users.noreply.github.com>
2024-03-14 07:21:36 +0700

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,20 @@
-public class Item {
+abstract class Item{
+    public String name;
+    private String description;
+
+    public String getDescription(){
+        return description;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
+    protected String ID;
+    int gia;
+    public abstract void showinfo();
+    public Item(String name, String description, String ID, int gia){
+        this.name = name;
+        this.description = description;
+        this.ID = ID;
+        this.gia = gia;
+    }
 }
