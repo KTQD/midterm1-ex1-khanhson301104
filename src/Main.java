bef28d37638beaaad8f51d693e9442ff2646df2f
khanhson301104 <52119462+khanhson301104@users.noreply.github.com>
2024-03-14 07:30:17 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        MP3 mp3;
-        mp3.showInfo();
-
-        Book book;
-        book.showInfo();
+        MP3 mp3 = new MP3("NCT", "nghenhac", "1", 1000, 20);
+        Book book = new Book("Aga", "Sach", "2", 2000,"Son",100,"Normal");
+        book.showinfo();
+        mp3.showinfo();
 
     }
 }
